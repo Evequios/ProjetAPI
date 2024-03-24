ee29411d1273a6529667cc3ceb83e71783dd6f21
Evequios <kilianbnr@outlook.fr>
2024-03-25 00:19:33 +0100

    Finishing client controller

--- clients/src/main/java/com/kb/api/controller/ClientController.java
+++ clients/src/main/java/com/kb/api/controller/ClientController.java
@@ -6,7 +6,12 @@ import java.util.stream.Collectors;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -37,4 +42,31 @@ public class ClientController {
                 linkTo(methodOn(ClientController.class).getClients()).withSelfRel()));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<Client>> getClient(@PathVariable int id) {
+        Client client = clientService.getClient(id);
+        return ResponseEntity.ok(EntityModel.of(client,
+                linkTo(methodOn(ClientController.class).getClient(id)).withSelfRel(),
+                linkTo(methodOn(ClientController.class).getClients()).withRel("clients")));
+    }
+
+    @PostMapping
+    public ResponseEntity<EntityModel<Client>> createClient(@RequestBody Client client) {
+        return ResponseEntity.ok(EntityModel.of(clientService.createClient(client),
+                linkTo(methodOn(ClientController.class).getClient(client.getId())).withSelfRel(),
+                linkTo(methodOn(ClientController.class).getClients()).withRel("clients")));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<Client>> updateClient(@PathVariable int id, @RequestBody Client client) {
+        client.setId(id);
+        return ResponseEntity.ok(EntityModel.of(clientService.updateClient(client),
+                linkTo(methodOn(ClientController.class).getClient(id)).withSelfRel(),
+                linkTo(methodOn(ClientController.class).getClients()).withRel("clients")));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteClient(@PathVariable int id) {
+        return clientService.deleteClient(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
 }
--- clients/src/main/java/com/kb/api/model/Client.java
+++ clients/src/main/java/com/kb/api/model/Client.java
@@ -2,6 +2,7 @@ package com.kb.api.model;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,11 +15,23 @@ public class Client {
     @Id
     @GeneratedValue
     private int id;
+
+    @Column(name = "lastname")
     private String lastName;
+
+    @Column(name = "firstname")
     private String firstName;
+
+    @Column(name = "birthdate")
     private Date birthDate;
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "job")
     private String job;
+
+    @Column(name = "income")
     private int income;
 
     public int getId() {
